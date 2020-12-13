0bb0fbffb4f08f4d4c6a32459e0cbf205b2b45ce
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-13 23:44:24 +0100

    Update twitter-uncrop.js

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -1,24 +1,26 @@
 (function() {
 	"use strict";
 
-	function getMainElementFromArticle(article) {
-		return article.querySelector(".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg");
+	function getMainImageContainerFromArticle(article) {
+		const query = article.querySelector(".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg");
+		if (query)
+			return query.parentElement; // todo https://github.com/jshint/jshint/issues/3448
 	}
 
-	function getRetweetElementFromArticle(article) {
-		return article.querySelector("div[class='css-1dbjc4n r-156q2ks']");
-	}
+	// function getRetweetElementFromArticle(article) {
+	// 	return article.querySelector("div[class='css-1dbjc4n r-156q2ks']");
+	// }
 
 	setInterval(function() {
 		const articles = document.querySelectorAll("article:not([data-uncropped])");
 
 		for (let article of articles) {
-			const mainElement = getMainElementFromArticle(article);
-			const images = mainElement.querySelectorAll("img[alt=Image]");
-
-			if (images.length == 0) {
-				return;
+			const imageContainer = getMainImageContainerFromArticle(article);
+			if (!imageContainer) {
+				continue;
 			}
+
+			// const images = imageContainer.querySelectorAll("img[alt=Image]");
 
 			// let aAndImageURLs = [];
 
@@ -36,7 +38,7 @@
 			// 	);
 			// }
 
-			mainElement.textContent = "TEST TEST TEST TEST TEST TEST TEST TEST";
+			imageContainer.textContent = "TEST TEST TEST TEST TEST TEST TEST TEST";
 
 			// for (let x of aAndImageURLs) {
 			// 	let img = document.createElement("img");
@@ -47,7 +49,5 @@
 
 			article.setAttribute("data-uncropped", "");
 		}
-
 	}, 400);
-
 })();
