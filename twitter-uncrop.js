f86403f941b717115869e8dfe7cca774b0b9d478
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-13 22:52:31 +0100

    Update twitter-uncrop.js

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -1,58 +1,42 @@
 (function() {
 	"use strict";
 
+	function getMainElementFromArticle(article) {
+		return article.querySelector(".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg");
+	}
+
 	function getRetweetElementFromArticle(article) {
-		return Array.from(article.querySelectorAll("div")).filter(
-			function(e) {
-				let arr = Array.from(e.classList);
-				if (arr.length == 2 && arr.includes("css-1dbjc4n") && arr.includes("r-156q2ks")) {
-					return true;
-				}
-			}
-		)[0];
+		return article.querySelector("div[class='css-1dbjc4n r-156q2ks']");
 	}
-
-	function getParentContainerFromImage(image) { // is either main tweet content or retweet content
-
-	}
-
-	// .css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg
-	// document.querySelectorAll("div[class='css-1dbjc4n r-156q2ks']")
 
 	setInterval(function() {
 		const articles = document.querySelectorAll("article:not([data-uncropped])");
 
 		for (let article of articles) {
-			// article.setAttribute("style", "opacity: 0.1");
-
-			const images = article.querySelectorAll("img[alt=Image]");
+			const mainElement = getMainElementFromArticle(article);
+			const images = mainElement.querySelectorAll("img[alt=Image]");
 
 			if (images.length == 0) {
 				return;
 			}
 
-			let aAndImageURLs = [];
+			// let aAndImageURLs = [];
 
-			for (let image of images) {
-				const a = image.closest("a");
-				if (!a) {
-					continue;
-				}
+			// for (let image of images) {
+			// 	const a = image.closest("a");
+			// 	if (!a) {
+			// 		continue;
+			// 	}
 
-				aAndImageURLs.push(
-					{
-						a: a,
-						imageUrl: image.src
-					}
-				);
-			}
+			// 	aAndImageURLs.push(
+			// 		{
+			// 			a: a,
+			// 			imageUrl: image.src
+			// 		}
+			// 	);
+			// }
 
-			// r-18bvks7
-			// css-1dbjc4n r-156q2ks
-			// Array.from(document.querySelectorAll("div")).filter(function(e){let arr = Array.from(e.classList); if(arr.length==2 && arr.includes("css-1dbjc4n") && arr.includes("r-156q2ks")) {return true;}})
-
-			let contentArea = aAndImageURLs[0].a.closest("div[class*=r-156q2ks][class*=r-18bvks7]").parentElement;
-			contentArea.textContent = "TEST TEST TEST TEST TEST TEST TEST TEST";
+			mainElement.textContent = "TEST TEST TEST TEST TEST TEST TEST TEST";
 
 			// for (let x of aAndImageURLs) {
 			// 	let img = document.createElement("img");
