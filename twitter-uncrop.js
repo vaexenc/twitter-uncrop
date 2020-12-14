37d63669cedff995aba68af48a2892f69ebb3c1a
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-14 03:15:06 +0100

    Revert "jslint"
    This reverts commit 814d9ebe4cba50d0f73ce67c118b7c04aa06fe7b.

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -1,9 +1,9 @@
 (function() {
-	// "use strict";
+	"use strict";
 
 	function getMainImageContainerFromArticle(article) {
 		const query = article.querySelector(".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg");
-		if (query) {return query.parentElement;} // todo https://github.com/jshint/jshint/issues/3448
+		if (query) return query.parentElement; // todo https://github.com/jshint/jshint/issues/3448
 	}
 
 	// function getRetweetContainerFromArticle(article) {
@@ -13,16 +13,16 @@
 	setInterval(function() {
 		const articles = document.querySelectorAll("article:not([data-uncropped])");
 
-		articles.forEach(function(article) {
+		for (let article of articles) {
 			const imageContainer = getMainImageContainerFromArticle(article);
-			if (!imageContainer) {return undefined;}
+			if (!imageContainer) continue;
 
 			const images = imageContainer.querySelectorAll("img[alt=Image]");
-			if (images.length === 0) {return undefined;} // if they haven't been added yet
+			if (images.length == 0) continue; // if they haven't been added yet
 
 			const aAndImageURLs = [];
 
-			images.forEach(function(image) {
+			for (let image of images) {
 				// const a = image.closest("a");
 				aAndImageURLs.push(
 					{
@@ -30,18 +30,18 @@
 						imageUrl: image.src.replace(/name=\w+/, "name=orig")
 					}
 				);
-			});
+			}
 
 			imageContainer.textContent = "";
 
-			aAndImageURLs.forEach(function(aAndImageURL) {
+			for (let aAndImageURL of aAndImageURLs) {
 				let img = document.createElement("img");
 				img.src = aAndImageURL.imageUrl;
 				img.style = "width: 100%; margin-top: 5px; margin-bottom: 3px";
 				imageContainer.appendChild(img);
-			});
+			}
 
 			article.setAttribute("data-uncropped", "");
-		});
+		}
 	}, 600);
-}());
+})();
