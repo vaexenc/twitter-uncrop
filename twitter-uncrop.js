814d9ebe4cba50d0f73ce67c118b7c04aa06fe7b
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-14 03:12:23 +0100

    jslint

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -1,9 +1,9 @@
 (function() {
-	"use strict";
+	// "use strict";
 
 	function getMainImageContainerFromArticle(article) {
 		const query = article.querySelector(".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg");
-		if (query) return query.parentElement; // todo https://github.com/jshint/jshint/issues/3448
+		if (query) {return query.parentElement;} // todo https://github.com/jshint/jshint/issues/3448
 	}
 
 	// function getRetweetContainerFromArticle(article) {
@@ -13,16 +13,16 @@
 	setInterval(function() {
 		const articles = document.querySelectorAll("article:not([data-uncropped])");
 
-		for (let article of articles) {
+		articles.forEach(function(article) {
 			const imageContainer = getMainImageContainerFromArticle(article);
-			if (!imageContainer) continue;
+			if (!imageContainer) {return undefined;}
 
 			const images = imageContainer.querySelectorAll("img[alt=Image]");
-			if (images.length == 0) continue; // if they haven't been added yet
+			if (images.length === 0) {return undefined;} // if they haven't been added yet
 
 			const aAndImageURLs = [];
 
-			for (let image of images) {
+			images.forEach(function(image) {
 				// const a = image.closest("a");
 				aAndImageURLs.push(
 					{
@@ -30,18 +30,18 @@
 						imageUrl: image.src.replace(/name=\w+/, "name=orig")
 					}
 				);
-			}
+			});
 
 			imageContainer.textContent = "";
 
-			for (let aAndImageURL of aAndImageURLs) {
+			aAndImageURLs.forEach(function(aAndImageURL) {
 				let img = document.createElement("img");
 				img.src = aAndImageURL.imageUrl;
 				img.style = "width: 100%; margin-top: 5px; margin-bottom: 3px";
 				imageContainer.appendChild(img);
-			}
+			});
 
 			article.setAttribute("data-uncropped", "");
-		}
+		});
 	}, 600);
-})();
+}());
