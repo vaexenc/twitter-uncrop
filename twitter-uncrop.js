8745f29b5fa70e783a11f2c517515815eaf49e88
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-17 20:15:21 +0100

    made it look more seamless

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -15,6 +15,7 @@
 
 	const APPROXIMATE_MAIN_IMAGE_CONTAINER_SELECTOR = ".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg";
 	const IMAGE_STYLE = "width: 100%; margin-top: 5px; margin-bottom: 3px";
+	const APPROXIMATE_MAIN_IMAGE_CONTAINER_STYLE = "opacity: 0; height: 0px; border-width: 0px; margin-top: 0px;";
 
 	function getMainImageContainerFromApproximateMainImageContainer(imageContainer) {
 		return imageContainer.parentElement;
@@ -53,10 +54,12 @@
 				const imageA = image.closest("a");
 				const approximateMainImageContainer = imageA.closest(APPROXIMATE_MAIN_IMAGE_CONTAINER_SELECTOR);
 				const mainImageContainer = getMainImageContainerFromApproximateMainImageContainer(approximateMainImageContainer);
-				setTimeout(function() { // images don't get added if it's already hidden?
-					approximateMainImageContainer.style.setProperty("display", "none");
-					approximateMainImageContainer.hidden = "true"; // ?
-				}, 0);
+				setTimeout(function() {
+					// approximateMainImageContainer.style.setProperty("display", "none"); // images don't get added if it's already hidden?
+					// approximateMainImageContainer.hidden = "true"; // unnecessary?
+					approximateMainImageContainer.remove();
+				}, 100);
+				approximateMainImageContainer.style.cssText += APPROXIMATE_MAIN_IMAGE_CONTAINER_STYLE;
 				sanitizeImageA(imageA);
 				mainImageContainer.append(imageA);
 				imageA.append(newImage);
