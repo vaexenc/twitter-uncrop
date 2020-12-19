93db5b54d1b690192a3ff6b50ae93c1080989add
vaexenc <53697802+vaexenc@users.noreply.github.com>
2020-12-19 18:39:10 +0100

    refac

--- twitter-uncrop.js
+++ twitter-uncrop.js
@@ -14,7 +14,7 @@
 	"use strict";
 
 	// const OPTION_RETWEETS = true;
-	
+
 	const APPROXIMATE_MAIN_IMAGE_CONTAINER_SELECTOR = ".css-1dbjc4n.r-18bvks7.r-1867qdf.r-1phboty.r-rs99b7.r-156q2ks.r-1ny4l3l.r-1udh08x.r-o7ynqc.r-6416eg";
 	// const RETWEET_SELECTOR = "div[class='css-1dbjc4n r-156q2ks']";
 	// const RETWEET_CONTENT_SELECTOR = "div[class='css-1dbjc4n r-18u37iz']"
@@ -50,6 +50,33 @@
 	// 	return article.querySelector("div[class='css-1dbjc4n r-156q2ks']");
 	// }
 
+	function imageArraySortFunction(image1, image2) {
+		const image1OrderNumber = getOrderNumberFromImage(image1);
+		const image2OrderNumber = getOrderNumberFromImage(image2);
+		if (image1OrderNumber < image2OrderNumber) return -1;
+		return 1;
+	}
+
+	function doTheImageThing(imageNode) {
+		const approximateMainImageContainer = imageNode.closest(APPROXIMATE_MAIN_IMAGE_CONTAINER_SELECTOR);
+		if (!approximateMainImageContainer || approximateMainImageContainer.hasAttribute(APPROXIMATE_MAIN_IMAGE_CONTAINER_CUSTOM_ATTRIBUTE)) return;
+		approximateMainImageContainer.style.cssText += APPROXIMATE_MAIN_IMAGE_CONTAINER_STYLE;
+		approximateMainImageContainer.setAttribute(APPROXIMATE_MAIN_IMAGE_CONTAINER_CUSTOM_ATTRIBUTE, "");
+		setTimeout(function() {
+			const mainImageContainer = getMainImageContainerFromApproximateMainImageContainer(approximateMainImageContainer);
+			const images = Array.from(approximateMainImageContainer.querySelectorAll("img"));
+			images.sort(imageArraySortFunction);
+			for (const image of images) {
+				const newImage = createImage(getHighQualityImageURLfromImage(image));
+				const imageA = image.closest("a");
+				sanitizeImageA(imageA);
+				mainImageContainer.append(imageA);
+				imageA.append(newImage);
+			}
+			approximateMainImageContainer.remove();
+		}, 0);
+	}
+
 	const observerTarget = document.querySelector("html");
 	const observerConfig = {childList: true, subtree: true};
 
@@ -58,28 +85,7 @@
 			if (!mutation.addedNodes) return;
 			for (const addedNode of mutation.addedNodes) {
 				if (addedNode.tagName !== "IMG" || !addedNode.src.includes("&name=")) continue;
-				const approximateMainImageContainer = addedNode.closest(APPROXIMATE_MAIN_IMAGE_CONTAINER_SELECTOR);
-				if (!approximateMainImageContainer || approximateMainImageContainer.hasAttribute(APPROXIMATE_MAIN_IMAGE_CONTAINER_CUSTOM_ATTRIBUTE)) continue;
-				approximateMainImageContainer.style.cssText += APPROXIMATE_MAIN_IMAGE_CONTAINER_STYLE;
-				approximateMainImageContainer.setAttribute(APPROXIMATE_MAIN_IMAGE_CONTAINER_CUSTOM_ATTRIBUTE, "");
-				setTimeout(function() {
-					const mainImageContainer = getMainImageContainerFromApproximateMainImageContainer(approximateMainImageContainer);
-					const images = Array.from(approximateMainImageContainer.querySelectorAll("img"));
-					images.sort(function(image1, image2) {
-						const image1OrderNumber = getOrderNumberFromImage(image1);
-						const image2OrderNumber = getOrderNumberFromImage(image2);
-						if (image1OrderNumber < image2OrderNumber) return -1;
-						return 1;
-					});
-					for (const image of images) {
-						const newImage = createImage(getHighQualityImageURLfromImage(image));
-						const imageA = image.closest("a");
-						sanitizeImageA(imageA);
-						mainImageContainer.append(imageA);
-						imageA.append(newImage);
-					}
-					approximateMainImageContainer.remove();
-				}, 0);
+				doTheImageThing(addedNode);
 			}
 		}
 	};
